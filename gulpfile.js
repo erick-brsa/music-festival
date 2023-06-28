d87f63d3ecca2382f6d9289fa1c400446d45a4e3
Erick Briones <erikalan518@gmail.com>
2023-06-27 22:34:38 -0600

    Declarando variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
+const { src, dest, watch } = require("gulp")
+const sass = require('gulp-sass')(require('sass'))
+const plumber = require('gulp-pumber')
+
 function css(done) {
     // Identifcar el archivo de SASS
+    src('src/scss/**/*.scss')
 
+    // dependencia para debuggear
+    .pipe(plumber())
+    
     // Compilarlo
+    .pipe(sass())
     
     // Almacenar
+    .pipe(dest('build/css'))   
 
     done() // Callback quavisa que avisa a gulp cuando llegams al final
 }
 
-exports.task = css
+function dev(done) {
+    watch('src/scss/app.scss', css)
+    done()
+}
+
+exports.css = css
+exports.dev = dev
